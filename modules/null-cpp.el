6dc6492e8eee89a2bd2c4dce0983fe20930cfc84
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2023-01-02 09:03:04 +0100

    cpp & python modules

---
+++ modules/null-cpp.el
@@ -0,0 +1,87 @@
+;;; null-cpp.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Christoffer Arvidsson
+;; Maintainer: Christoffer Arvidsson
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+;; Lsp 
+(use-package ccls
+  :after lsp
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+;; Mode
+(use-package cc-mode
+  :ensure t
+  :config 
+  (setq c-tab-always-indent t)
+  (setq-default c-basic-offset 4)
+  (c-add-style
+   "doom" '((c-comment-only-line-offset . 0)
+            (c-hanging-braces-alist (brace-list-open)
+                                    (brace-entry-open)
+                                    (substatement-open after)
+                                    (block-close . c-snug-do-while)
+                                    (arglist-cont-nonempty))
+            (c-cleanup-list brace-else-brace)
+            (c-offsets-alist
+             (knr-argdecl-intro . 0)
+             (substatement-open . 0)
+             (substatement-label . 0)
+             (statement-cont . +)
+             (case-label . +)
+             ;; align args with open brace OR don't indent at all (if open
+             ;; brace is at eolp and close brace is after arg with no trailing
+             ;; comma)
+             (brace-list-intro . 0)
+             (brace-list-close . -)
+             (arglist-intro . +)
+             (arglist-close +cc-lineup-arglist-close 0)
+             ;; don't over-indent lambda blocks
+             (inline-open . 0)
+             (inlambda . 0)
+             ;; indent access keywords +1 level, and properties beneath them
+             ;; another level
+             (access-label . -)
+             (inclass +cc-c++-lineup-inclass +)
+             (label . 0))))
+
+  (when (listp c-default-style)
+    (setf (alist-get 'other c-default-style) "doom")))
+
+;; Cuda
+(use-package cuda-mode
+  :ensure t
+  :config
+  (setq auto-mode-alist
+        (cons '("\\.cu$" . cuda-mode) auto-mode-alist)))
+
+(provide 'null-cpp)
+
+;;; null-cpp.el ends here
---
+++ modules/null-python.el
@@ -0,0 +1,95 @@
+;;; null-python.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Christoffer Arvidsson
+;; Maintainer: Christoffer Arvidsson
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(require 'null-keybinds)
+
+(use-package python
+  :defer t
+  :custom
+  (python-indent-offset 4))
+
+(use-package anaconda-mode
+  :defer t
+  :hook ((python-mode . anaconda-mode)
+         (python-mode . anaconda-eldoc-mode))
+  :config
+  (add-to-list 'company-backends 'company-anaconda)
+  (add-to-list 'python-shell-extra-pythonpaths "~/repos/madame_web"))
+
+(use-package lsp-pyright
+  :after lsp
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp-deferred))))  ; or lsp-deferred
+
+;; Handle different python versions
+(use-package pyenv-mode
+  :ensure t
+  :defer t
+  :config
+  (pyenv-mode))
+
+;; Remove unused imports on save
+(use-package pyimport
+  :ensure t
+  :config
+  (add-hook 'before-save-hook 'pyimport-remove-unused))
+
+;; Sort imports on save
+(use-package py-isort
+  :ensure t
+  :config
+  (add-hook 'before-save-hook 'py-isort-before-save))
+
+;; Handle venvs
+(use-package pyvenv
+  :config
+  (pyvenv-mode 1))
+
+;; Format buffer on save
+(use-package python-black
+  :demand t
+  :after python
+  :hook (python-mode . python-black-on-save-mode-enable-dwim))
+
+(null-keybinds-major-key-def
+  :states '(normal visual)
+  :keymaps 'python-mode-map
+  "r" 'run-python
+  "c" 'python-shell-send-buffer
+  "f" 'python-shell-send-defun
+  "v" 'python-shell-send-region)
+
+(provide 'null-python)
+
+;;; null-python.el ends here
