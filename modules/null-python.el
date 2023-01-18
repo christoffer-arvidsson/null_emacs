985eebfd20d137b1ea2921a1af3ddf0f58d9c29f
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-01-18 20:17:41 +0100

    Make evil consider words_words as one symbol

--- modules/null-python.el
+++ modules/null-python.el
@@ -37,7 +37,10 @@
   :custom
   (python-indent-offset 4)
   :config
-  (setenv "PYTHONPATH" "/home/s0001520/repos/madame-web"))
+  (setenv "PYTHONPATH" "/home/s0001520/repos/madame-web")
+
+  ;; Make evil treat words_words as one symbol
+  (add-hook 'python-mode-hook #'(lambda () (modify-syntax-entry ?_ "w"))))
 
 (use-package anaconda-mode
   :defer t
