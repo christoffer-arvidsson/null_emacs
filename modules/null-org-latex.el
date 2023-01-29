9d2ae8c0344a0db689ebebfdb6cd54c75b84b38b
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-01-29 21:55:12 +0100

    Add latex

---
+++ modules/null-org-latex.el
@@ -0,0 +1,117 @@
+;;; null-org-latex.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Christoffer Arvidsson
+;; Maintainer: Christoffer Arvidsson
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+
+(require 'null-org)
+
+;; (defvar org--font-locking-latex-fragment nil)
+
+;; (defadvice signal-font-locking-latex (orig-fun &rest args)
+;;   :around #'org-do-latex-and-related
+;;   (let ((org--font-locking-latex-fragment t)
+;;         (apply orig-fun args))))
+
+;; (defadvice do-not-block-my-latex-advice (_ start end)
+;;   :after #'org-src-font-lock-fontify-block
+;;   (when org--font-locking-latex-fragment
+;;     (alter-text-property start end 'face (lambda (l) (remove 'org-block l)))))
+
+;; Latex
+(use-package org-fragtog
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook 'org-fragtog-mode)
+  (add-hook 'org-mode-hook 'update-org-latex-fragment-scale)
+  (setq org-fragtog-ignore-predicates '(org-at-table-p)))
+
+;; Numbered equations all have (1) as the number for fragments with vanilla
+;; org-mode. This code injects the correct numbers into the previews so they
+;; look good.
+(defun scimax-org-renumber-environment (orig-func &rest args)
+  "A function to inject numbers in LaTeX fragment previews."
+  (let ((results '())
+        (counter -1)
+        (numberp))
+    (setq results (cl-loop for (begin . env) in
+                           (org-element-map (org-element-parse-buffer) 'latex-environment
+                             (lambda (env)
+                               (cons
+                                (org-element-property :begin env)
+                                (org-element-property :value env))))
+                           collect
+                           (cond
+                            ((and (string-match "\\\\begin{equation}" env)
+                                  (not (string-match "\\\\tag{" env)))
+                             (cl-incf counter)
+                             (cons begin counter))
+                            ((string-match "\\\\begin{align}" env)
+                             (prog2
+                                 (cl-incf counter)
+                                 (cons begin counter)
+                               (with-temp-buffer
+                                 (insert env)
+                                 (goto-char (point-min))
+                                 ;; \\ is used for a new line. Each one leads to a number
+                                 (cl-incf counter (count-matches "\\\\$"))
+                                 ;; unless there are nonumbers.
+                                 (goto-char (point-min))
+                                 (cl-decf counter (count-matches "\\nonumber")))))
+                            (t
+                             (cons begin nil)))))
+
+    (when (setq numberp (cdr (assoc (point) results)))
+      (setf (car args)
+            (concat
+             (format "\\setcounter{equation}{%s}\n" numberp)
+             (car args)))))
+
+  (apply orig-func args))
+
+
+(defun scimax-toggle-latex-equation-numbering ()
+  "Toggle whether LaTeX fragments are numbered."
+  (interactive)
+  (if (not (get 'scimax-org-renumber-environment 'enabled))
+      (progn
+        (advice-add 'org-create-formula-image :around #'scimax-org-renumber-environment)
+        (put 'scimax-org-renumber-environment 'enabled t)
+        (message "Latex numbering enabled"))
+    (advice-remove 'org-create-formula-image #'scimax-org-renumber-environment)
+    (put 'scimax-org-renumber-environment 'enabled nil)
+    (message "Latex numbering disabled.")))
+
+(advice-add 'org-create-formula-image :around #'scimax-org-renumber-environment)
+(put 'scimax-org-renumber-environment 'enabled t)
+
+(provide 'null-org-latex)
+
+;;; null-org-latex.el ends here
