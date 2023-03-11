60ef71a05e4742cca419523b2a31c1a5c7220f4b
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-03-11 22:43:01 +0100

    Split windows sensibly

--- config.el
+++ config.el
@@ -52,6 +52,22 @@
 ;; Work
 (if (string-equal (system-name) "ucnd1387l61") (null/init-work-packages))
 
+(setq split-height-threshold 120
+      split-width-threshold 160)
+
+(defun my-split-window-sensibly (&optional window)
+    "replacement `split-window-sensibly' function which prefers vertical splits"
+    (interactive)
+    (let ((window (or window (selected-window))))
+        (or (and (window-splittable-p window t)
+                 (with-selected-window window
+                     (split-window-right)))
+            (and (window-splittable-p window)
+                 (with-selected-window window
+                     (split-window-below))))))
+
+(setq split-window-preferred-function #'my-split-window-sensibly)
+
 (provide 'config)
 ;;; config.el ends here
 
