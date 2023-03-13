c1d372969ac797e41f5b5c8b1df15f6cc6a6dfff
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-03-13 20:40:37 +0100

    Fix bootstrap

--- bootstrap/null-straight-bootstrap.el
+++ bootstrap/null-straight-bootstrap.el
@@ -1,12 +1,12 @@
+(defvar native-comp-deferred-compilation-deny-list nil)
 (defvar bootstrap-version)
-
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straigh.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
