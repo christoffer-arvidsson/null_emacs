957a2a6c7fe343d5cc3e2c40ec095dde681ac41a
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-03-14 19:12:03 +0100

    Update smerge hydra

--- modules/null-project.el
+++ modules/null-project.el
@@ -35,39 +35,46 @@
   (magit-todos-mode))
 
 (use-package hydra)
+
 (use-package smerge-mode
+  :after hydra
   :config
-  (defhydra scimax-smerge (:color red :hint nil)
+  (defhydra unpackaged/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
     "
-Navigate       Keep               other
-----------------------------------------
-_p_: previous  _c_: current       _e_: ediff
-_n_: next      _m_: mine  <<      _u_: undo
-_j_: up        _o_: other >>      _r_: refine
-_k_: down      _a_: combine       _q_: quit
-               _b_: base
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
 "
     ("n" smerge-next)
     ("p" smerge-prev)
-    ("c" smerge-keep-current)
-    ("m" smerge-keep-mine)
-    ("o" smerge-keep-other)
     ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
     ("a" smerge-keep-all)
-    ("e" smerge-ediff)
-    ("j" previous-line)
-    ("k" forward-line)
-    ("r" smerge-refine)
-    ("u" undo)
-    ("q" nil :exit t))
-
-  (defun enable-smerge-maybe ()
-    (when (and buffer-file-name (vc-backend buffer-file-name))
-      (save-excursion
-        (goto-char (point-min))
-        (when (re-search-forward "^<<<<<<< " nil t)
-          (smerge-mode +1)
-          (scimax-smerge/body))))))
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (unpackaged/smerge-hydra/body)))))
 
 (use-package git-gutter-fringe
   :config
