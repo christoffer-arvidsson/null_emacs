0b78497002adea617828e3a33951daefedb5b26b
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-05-20 23:21:31 +0200

    Add drag-stuff mode

--- modules/null-editor.el
+++ modules/null-editor.el
@@ -69,6 +69,11 @@
   :config
   (evil-multiedit-default-keybinds))
 
+(use-package drag-stuff
+  :config
+  (drag-stuff-global-mode +1)
+  (drag-stuff-define-keys))
+
 (use-package avy
   :custom
   (avy-timeout-seconds 0.3)
