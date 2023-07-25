e9eda5cada80a7f50fe00c4c833277860b3cda99
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-07-25 10:36:31 +0200

    Fix 4 month old typo in null-straight-bootstrap

--- bootstrap/null-straight-bootstrap.el
+++ bootstrap/null-straight-bootstrap.el
@@ -1,13 +1,14 @@
+(setq package-enable-at-startup nil)
 (defvar native-comp-deferred-compilation-deny-list nil)
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straigh.el/develop/install.el"
-         'silent 'inhibit-cookies)
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
