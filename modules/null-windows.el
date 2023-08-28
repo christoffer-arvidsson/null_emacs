55b8d359f04ea9192bd0688500c1b552a1f5118b
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-08-28 21:10:08 +0200

    Add embark live window rule

--- modules/null-windows.el
+++ modules/null-windows.el
@@ -168,6 +168,7 @@ the only window, use evil-window-move-* (e.g. `evil-window-move-far-left')."
         '(("*org-roam*"                    :align right  :size 0.3    :select t)
           ("*org-roam-review*"             :align right  :size 0.5    :select t)
           ("\\*Embark Export:"             :regexp t   :align below :size 0.25 :select t)
+          ("\\*Embark Live:"               :regexp t   :align right :size 0.5 :select t)
           (embark-collect-mode             :regexp nil :align below :size 0.25 :select t)
           ("*Org Links*"                   :noselect nil :size 0.1)
           ("*Org Export Process*"          :noselect t   :size 0.25 :align below)
