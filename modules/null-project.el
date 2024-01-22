c523a013382d18d8c4fbaf58ee7c91ccea1f86f1
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2024-01-22 08:37:26 +0100

    Add compile colors

--- modules/null-project.el
+++ modules/null-project.el
@@ -26,6 +26,28 @@
   :custom
   (dumb-jump-prefer-searcher 'rg))
 
+(use-package compile
+  :elpaca nil ; built-in
+  :config
+  ;; Handle ansi characters in compilation buffer
+  ;; https://emacs.stackexchange.com/questions/8135/why-does-compilation-buffer-show-control-characters
+  (require 'ansi-color)
+  (defun my/ansi-colorize-buffer ()
+    (let ((buffer-read-only nil))
+      (ansi-color-apply-on-region (point-min) (point-max))))
+  (add-hook 'compilation-filter-hook 'my/ansi-colorize-buffer))
+
+(use-package project
+  :elpaca nil ; built-in
+  :config
+  ;; Handle ansi characters in compilation buffer
+  ;; https://emacs.stackexchange.com/questions/8135/why-does-compilation-buffer-show-control-characters
+  (require 'ansi-color)
+  (defun my/ansi-colorize-buffer ()
+    (let ((buffer-read-only nil))
+      (ansi-color-apply-on-region (point-min) (point-max))))
+  (add-hook 'compilation-filter-hook 'my/ansi-colorize-buffer))
+
 (use-package project
   :elpaca nil ; built-in
   :config
