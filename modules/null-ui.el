832a718b5aaf01f4b18985ad668cbd5ce0699885
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-02-25 20:44:50 +0100

    [ui] docs

--- modules/null-ui.el
+++ modules/null-ui.el
@@ -1,4 +1,31 @@
-;;; null-ui.el -*- lexical-binding: t; -*-
+;;; null-ui.el --- Ui configuration -*- lexical-binding: t -*-
+
+;; Author: Christoffer Arvidsson
+;; Maintainer: Christoffer Arvidsson
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'null-keybinds)
 (require 'null-font)
@@ -81,3 +108,5 @@
   "t b" '(lambda () (interactive) (null-global-big-text-mode 'toggle)))
 
 (provide 'null-ui)
+
+;;; null-ui.el ends here
