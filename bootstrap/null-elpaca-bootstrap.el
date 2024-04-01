d2944c601d3aecaec6605800a7c0cc562bf469a1
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-04-01 16:16:39 +0200

    Swap over to wip branch for org9.7

--- bootstrap/null-elpaca-bootstrap.el
+++ bootstrap/null-elpaca-bootstrap.el
@@ -48,9 +48,35 @@
   ;; Assume : t unless otherwise specified.
   (setq elpaca-use-package-by-default t))
 
-(elpaca org)
-
 ;; Block until current queue processed.
+(elpaca-wait)
+
+(use-package org
+  :defer
+  :ensure `(org
+            :remotes ("tecosaur"
+                      :repo "https://git.tecosaur.net/tec/org-mode.git"
+                      :branch "dev")
+            :files (:defaults "etc")
+            :build t
+            :pre-build
+            (with-temp-file "org-version.el"
+             (require 'lisp-mnt)
+             (let ((version
+                    (with-temp-buffer
+                      (insert-file-contents "lisp/org.el")
+                      (lm-header "version")))
+                   (git-version
+                    (string-trim
+                     (with-temp-buffer
+                       (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+                       (buffer-string)))))
+              (insert
+               (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+               (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+               "(provide 'org-version)\n")))
+            :pin nil))
+
 (elpaca-wait)
 
 (provide 'null-package/elpaca)
--- modules/null-org-latex.el
+++ modules/null-org-latex.el
@@ -33,43 +33,40 @@
 
 (require 'null-org)
 
-;; (defun my-latex-export-example-blocks (text backend info)
-;;   "Export example blocks as listings env."
-;;   (when (org-export-derived-backend-p backend 'latex)
-;;     (with-temp-buffer
-;;       (insert text)
-;;       ;; replace verbatim env by listings
-;;       (goto-char (point-min))
-;;       (replace-string "\\begin{verbatim}" "\\begin{lstlisting}")
-;;       (replace-string "\\end{verbatim}" "\\end{lstlisting}")
-;;       (buffer-substring-no-properties (point-min) (point-max)))))
+;; (defun null/setup-minted ()
+;;   (setq org-latex-listings 'minted
+;;         org-latex-custom-lang-environments '((emacs-lisp "common-lispcode"))
+;;         org-latex-minted-options
+;;         '(("bgcolor" "bgcode")
+;;           ("fontsize" "\\scriptsize")
+;;           ("baselinestretch" "0.9")
+;;           ("framesep" "3mm")
+;;           ("breaklines" "true")
+;;           ("linenos" "true")
+;;           ("numbersep" "2mm")
+;;           ("xleftmargin" "6mm"))))
 
-;; (add-to-list 'org-export-filter-example-block-functions
-;;          'my-latex-export-example-blocks)
+;; Breaks org-latex-preview if not loader here
+(use-package citeproc
+  :after org)
 
-(defun null/setup-minted ()
-  (setq org-latex-listings 'minted
-        org-latex-custom-lang-environments '((emacs-lisp "common-lispcode"))
-        org-latex-minted-options
-        '(("bgcolor" "bgcode")
-          ("fontsize" "\\scriptsize")
-          ("baselinestretch" "0.9")
-          ("framesep" "3mm")
-          ("breaklines" "true")
-          ("linenos" "true")
-          ("numbersep" "2mm")
-          ("xleftmargin" "6mm"))))
-
-;; Scale up latex fragments
 (with-eval-after-load 'org
-  (null/setup-minted)
+  ;; (null/setup-minted)
   (setq org-latex-default-class "orbit"
-        org-latex-packages-alist '(("" "minted"))
+        ;; org-latex-packages-alist '(("" "minted"))
         org-cite-export-processors '((t csl))
         org-latex-tables-booktabs t
-        org-latex-pdf-process '("LC_ALL=en_US.UTF-8 latexmk -f -pdf -%latex -shell-escape -interaction=nonstopmode -output-directory=%o %f"))
+        org-latex-pdf-process '("LC_ALL=en_US.UTF-8 latexmk -f -pdf -%latex -shell-escape -interaction=nonstopmode -output-directory=%o %f"cita))
 
-  (plist-put org-format-latex-options :scale 1.0))
+
+  (setq org-latex-packages-alist
+        '(("T1" "fontenc" t)
+          ("" "amsmath" t)
+          ("" "bm" t) ; Bold math required
+          ("" "mathtools" t)
+          ("" "siunitx" t)
+          ("" "algpseudocode" t)
+          ("" "algorithm" t))))
 
 (with-eval-after-load 'ox-latex
   (add-to-list 'org-latex-classes
@@ -79,6 +76,7 @@
 \\usepackage[dvipsnames]{xcolor}
 \\definecolor{bgcode}{rgb}{0.95,0.95,0.95}
 
+\\usepackage{amsmath}
 \\usepackage{booktabs}
 \\usepackage{gentium}
 \\usepackage{fancyhdr}
@@ -91,7 +89,6 @@
 \\usepackage{esdiff}
 \\usepackage[parfill]{parskip}
 \\usepackage{pgf}
-\\usepackage{minted}
 \\usemintedstyle{trac}
 \\usepackage{fancyvrb}
 \\usepackage{fvextra}
@@ -123,38 +120,19 @@
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-;; Latex
-(use-package org-fragtog
-  :hook (org-mode . org-fragtog-mode)
+(use-package org-latex-preview
+  :after (org citeproc)
+  :ensure nil
   :config
-  (setq org-fragtog-ignore-predicates '(org-at-table-p)))
+  (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
+  (add-hook 'org-latex-preview-auto-ignored-commands 'next-line)
+  (add-hook 'org-latex-preview-auto-ignored-commands 'previous-line)
+  (setq org-latex-preview-process-default 'dvisvgm
+        org-latex-preview-live-debounce 0.25
+        org-latex-preview-numbered t
+        org-latex-preview-live t
+        org-startup-with-latex-preview t))
 
-(setq org-preview-latex-default-process 'dvisvgm) ;No blur when scaling
-
-(defun my/text-scale-adjust-latex-previews ()
-  "Adjust the size of latex preview fragments when changing the
-buffer's text scale."
-  (pcase major-mode
-    ('latex-mode
-     (dolist (ov (overlays-in (point-min) (point-max)))
-       (if (eq (overlay-get ov 'category)
-               'preview-overlay)
-           (my/text-scale--resize-fragment ov))))
-    ('org-mode
-     (dolist (ov (overlays-in (point-min) (point-max)))
-       (if (eq (overlay-get ov 'org-overlay-type)
-               'org-latex-overlay)
-           (my/text-scale--resize-fragment ov))))))
-
-(defun my/text-scale--resize-fragment (ov)
-  (overlay-put
-   ov 'display
-   (cons 'image
-         (plist-put
-          (cdr (overlay-get ov 'display))
-          :scale (+ 1.0 (* 0.25 text-scale-mode-amount))))))
-
-(add-hook 'text-scale-mode-hook #'my/text-scale-adjust-latex-previews)
 
 (use-package cdlatex
   :custom
@@ -162,65 +140,6 @@ buffer's text scale."
   :hook (org-mode . org-cdlatex-mode)
   :bind (:map cdlatex-mode-map
               ("<tab>" . cdlatex-tab)))
-
-;; Numbered equations all have (1) as the number for fragments with vanilla
-;; org-mode. This code injects the correct numbers into the previews so they
-;; look good.
-(defun scimax-org-renumber-environment (orig-func &rest args)
-  "A function to inject numbers in LaTeX fragment previews."
-  (let ((results '())
-        (counter -1)
-        (numberp))
-    (setq results (cl-loop for (begin . env) in
-                           (org-element-map (org-element-parse-buffer) 'latex-environment
-                             (lambda (env)
-                               (cons
-                                (org-element-property :begin env)
-                                (org-element-property :value env))))
-                           collect
-                           (cond
-                            ((and (string-match "\\\\begin{equation}" env)
-                                  (not (string-match "\\\\tag{" env)))
-                             (cl-incf counter)
-                             (cons begin counter))
-                            ((string-match "\\\\begin{align}" env)
-                             (prog2
-                                 (cl-incf counter)
-                                 (cons begin counter)
-                               (with-temp-buffer
-                                 (insert env)
-                                 (goto-char (point-min))
-                                 ;; \\ is used for a new line. Each one leads to a number
-                                 (cl-incf counter (count-matches "\\\\$"))
-                                 ;; unless there are nonumbers.
-                                 (goto-char (point-min))
-                                 (cl-decf counter (count-matches "\\nonumber")))))
-                            (t
-                             (cons begin nil)))))
-
-    (when (setq numberp (cdr (assoc (point) results)))
-      (setf (car args)
-            (concat
-             (format "\\setcounter{equation}{%s}\n" numberp)
-             (car args)))))
-
-  (apply orig-func args))
-
-
-(defun scimax-toggle-latex-equation-numbering ()
-  "Toggle whether LaTeX fragments are numbered."
-  (interactive)
-  (if (not (get 'scimax-org-renumber-environment 'enabled))
-      (progn
-        (advice-add 'org-create-formula-image :around #'scimax-org-renumber-environment)
-        (put 'scimax-org-renumber-environment 'enabled t)
-        (message "Latex numbering enabled"))
-    (advice-remove 'org-create-formula-image #'scimax-org-renumber-environment)
-    (put 'scimax-org-renumber-environment 'enabled nil)
-    (message "Latex numbering disabled.")))
-
-(advice-add 'org-create-formula-image :around #'scimax-org-renumber-environment)
-(put 'scimax-org-renumber-environment 'enabled t)
 
 (provide 'null-org-latex)
 
