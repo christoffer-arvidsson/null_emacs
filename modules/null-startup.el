05caa9e5be4704dbc515c26c29b144fe91324fe0
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-09-15 11:58:09 +0200

    Ugly-patch dashboard to consider 2-width icon sizes

--- modules/null-startup.el
+++ modules/null-startup.el
@@ -28,10 +28,53 @@
                      (agenda . 5)
                      (registers . 5)))
   :config
-  (dashboard-setup-startup-hook)
 
-  ;; Makes emacsclient default to the dashboard
-  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))
+  (defun dashboard-get-icon-str (heading &optional icon)
+    (let ((args `( :height   ,dashboard-heading-icon-height
+                   :v-adjust ,dashboard-heading-icon-v-adjust
+                   :face     dashboard-heading)))
+      (pcase heading
+        ("Recent Files:"
+         (apply #'dashboard-octicon (cdr (assoc 'recents dashboard-heading-icons)) args))
+        ("Bookmarks:"
+         (apply #'dashboard-octicon (cdr (assoc 'bookmarks dashboard-heading-icons)) args))
+        ((or "Agenda for today:"
+             "Agenda for the coming week:")
+         (apply #'dashboard-octicon (cdr (assoc 'agenda dashboard-heading-icons)) args))
+        ("Registers:"
+         (apply #'dashboard-octicon (cdr (assoc 'registers dashboard-heading-icons)) args))
+        ("Projects:"
+         (apply #'dashboard-octicon (cdr (assoc 'projects dashboard-heading-icons)) args))
+        ("List Directories:"
+         (apply #'dashboard-octicon (cdr (assoc 'ls-directories dashboard-heading-icons)) args))
+        ("List Files:"
+         (apply #'dashboard-octicon (cdr (assoc 'ls-files dashboard-heading-icons)) args))
+        (_
+         (if (null icon) " " icon)))))
 
-(provide 'null-startup)
+  (defun dashboard-insert-heading (heading &optional shortcut icon)
+    "Insert a widget HEADING in dashboard buffer, adding SHORTCUT, ICON if provided."
+    (when (and (dashboard-display-icons-p) dashboard-set-heading-icons)
+      (let ((icon-str (dashboard-get-icon-str heading icon)))
+        (insert icon-str)
+        (insert (make-string (+ 1 (string-width icon-str)) ?\s))))
+
+      ;; Insert heading after the icon
+      (insert (propertize heading 'face 'dashboard-heading))
+
+      ;; Turn the inserted heading into an overlay
+      (let ((ov (make-overlay (- (point) (length heading)) (point) nil t)))
+        (overlay-put ov 'display (or (cdr (assoc heading dashboard-item-names)) heading))
+        (overlay-put ov 'face 'dashboard-heading))
+
+      ;; Add the shortcut if provided
+      (when shortcut
+        (insert (format dashboard-heading-shorcut-format shortcut))))
+
+    (dashboard-setup-startup-hook)
+
+    ;; Makes emacsclient default to the dashboard
+    (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))
+
+  (provide 'null-startup)
 ;;; null-startup.el ends here
