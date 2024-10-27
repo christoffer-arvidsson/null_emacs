7e15767675f14cb8c38f622395b7d8ed8a03503d
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-10-27 22:25:48 +0100

    Improve defaults

--- modules/null-defaults.el
+++ modules/null-defaults.el
@@ -27,9 +27,21 @@
 
 ;; Save history
 (use-package savehist
+  :custom
+  (history-length 128)
   :ensure nil ; built-in
   :init
-  (savehist-mode))
+  (savehist-mode 1))
+
+(use-package saveplace
+  :ensure nil ; built-in
+  :config
+  (save-place-mode 1))
+
+(use-package autorevert
+  :ensure nil
+  :config
+  (global-auto-revert-mode 1))
 
 (global-display-fill-column-indicator-mode +1)
 
