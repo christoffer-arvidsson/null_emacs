73fdc6fb637b01ae893c6a85987045449c9b7b5d
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2025-06-24 19:41:25 +0200

    Bump elpaca to 0.11

--- bootstrap/null-elpaca-bootstrap.el
+++ bootstrap/null-elpaca-bootstrap.el
@@ -2,7 +2,7 @@
 
 (setq package-enable-at-startup nil)
 
-(defvar elpaca-installer-version 0.9)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -17,7 +17,7 @@
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
                   ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
@@ -37,7 +37,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
@@ -52,31 +52,31 @@
 ;; Block until current queue processed.
 (elpaca-wait)
 
-(use-package org
-  :defer t
-  :ensure `(org
-            :remotes ("tecosaur"
-                      :repo "https://git.tecosaur.net/tec/org-mode.git"
-                      :branch "dev")
-            :files (:defaults "etc")
-            :build t
-            :pre-build
-            (with-temp-file "org-version.el"
-             (require 'lisp-mnt)
-             (let ((version
-                    (with-temp-buffer
-                      (insert-file-contents "lisp/org.el")
-                      (lm-header "version")))
-                   (git-version
-                    (string-trim
-                     (with-temp-buffer
-                       (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
-                       (buffer-string)))))
-              (insert
-               (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
-               (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
-               "(provide 'org-version)\n")))
-            :pin nil))
+;; (use-package org
+;;   :defer t
+;;   ;; :ensure `(org
+;;   ;;           :remotes ("tecosaur"
+;;   ;;                     :repo "https://git.tecosaur.net/tec/org-mode.git"
+;;   ;;                     :branch "dev")
+;;   ;;           :files (:defaults "etc")
+;;   ;;           :build t
+;;   ;;           :pre-build
+;;   ;;           (with-temp-file "org-version.el"
+;;   ;;            (require 'lisp-mnt)
+;;   ;;            (let ((version
+;;   ;;                   (with-temp-buffer
+;;   ;;                     (insert-file-contents "lisp/org.el")
+;;   ;;                     (lm-header "version")))
+;;   ;;                  (git-version
+;;   ;;                   (string-trim
+;;   ;;                    (with-temp-buffer
+;;   ;;                      (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+;;   ;;                      (buffer-string)))))
+;;   ;;             (insert
+;;   ;;              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+;;   ;;              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+;;   ;;              "(provide 'org-version)\n")))
+;;   )
 
 (provide 'null-package/elpaca)
 
